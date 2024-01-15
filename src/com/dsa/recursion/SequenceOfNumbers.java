6ca158fa31216200929d52b5b0e5651f02da754b
Harish <harishharish3759@gmail.com>
2024-01-15 22:29:24 +0530

    Sequence of numbers in recursion

---
+++ src/com/dsa/recursion/SequenceOfNumbers.java
@@ -0,0 +1,14 @@
+package com.dsa.recursion;
+
+public class SequenceOfNumbers {
+    public static void main(String[] args) {
+        sequence(5);
+    }
+
+    private static void sequence(int n) {
+        if(n==0)
+            return;
+        sequence(n-1);
+        System.out.println(n);
+    }
+}
