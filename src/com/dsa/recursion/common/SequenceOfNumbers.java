3bda0baa8ba2cf95575771d0c339f17e1671e171
Harish <harishharish3759@gmail.com>
2024-01-24 22:52:42 +0530

    Recursion easy problems

--- src/com/dsa/recursion/common/SequenceOfNumbers.java
+++ src/com/dsa/recursion/common/SequenceOfNumbers.java
@@ -3,13 +3,23 @@ package com.dsa.recursion.common;
 //Uses linear recurrence
 public class SequenceOfNumbers {
     public static void main(String[] args) {
-        sequence(5);
+        sequence1(5);
+        sequence2(5);
     }
 
-    private static void sequence(int n) {
+    private static void sequence1(int n) {
         if(n==0)
             return;
-        sequence(n-1);
+        sequence1(n-1);
         System.out.println(n);
     }
+
+    private static void sequence2(int n) {
+        if(n==0)
+            return;
+        System.out.println(n);
+        sequence2(n-1);
+    }
+
+
 }
