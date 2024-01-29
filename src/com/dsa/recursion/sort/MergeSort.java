f19683e1428d070f3f18271286856035c501b9e0
Harish <harishharish3759@gmail.com>
2024-01-29 21:56:52 +0530

    Merge sort using recursion

---
+++ src/com/dsa/recursion/sort/MergeSort.java
@@ -0,0 +1,63 @@
+package com.dsa.recursion.sort;
+
+import java.util.Arrays;
+
+//Time complexity - O(N*(log(N)))
+//Space complexity - O(N)
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSort(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr, int s, int e) {
+        if(e-s == 1){
+            return;
+        }
+
+        int mid = (s+e) / 2;
+
+        mergeSort(arr, s, mid);
+        mergeSort(arr, mid, e);
+
+        merge(arr, s, mid, e);
+    }
+
+    private static void merge(int[] arr, int s, int mid, int e) {
+        //System.out.println(s+" "+mid+" "+e);
+        int[] mix = new int[e-s];
+        int i = s;
+        int j = mid;
+        int k=0;
+
+        while (i<mid && j<e){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }
+            else{
+                mix[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i<mid){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j<e){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(int l=0;l<mix.length;l++){
+            arr[s+l]=mix[l];
+        }
+    }
+
+}
