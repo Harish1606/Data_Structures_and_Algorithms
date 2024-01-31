0f183c7c53ec4a30bde9d3b50d2f9e750d092429
Harish <harishharish3759@gmail.com>
2024-01-31 23:25:46 +0530

    Remove character from a string using recursion

---
+++ src/com/dsa/recursion/string/RemoveCharacter.java
@@ -0,0 +1,23 @@
+package com.dsa.recursion.string;
+
+//Time complexity - O(N)
+//Space complexity - O(n)
+public class RemoveCharacter {
+    public static void main(String[] args) {
+        char c = 'a';
+        String s = "afagecaabbda";
+        System.out.println(remove(s, c));
+    }
+
+    private static String remove(String s, char c) {
+        if(s.isEmpty()){
+            return "";
+        }
+        if(s.charAt(0) == c){
+            return remove(s.substring(1), c);
+        }
+        else{
+            return s.charAt(0) + remove(s.substring(1), c);
+        }
+    }
+}
