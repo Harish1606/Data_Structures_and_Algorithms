1b4bb8c30cd1327b1791f05d85c418ad7cce4065
Harish <harishharish3759@gmail.com>
2024-01-31 23:26:02 +0530

    Remove letter from a string using recursion

---
+++ src/com/dsa/recursion/string/RemoveLetter.java
@@ -0,0 +1,23 @@
+package com.dsa.recursion.string;
+
+//Time complexity - O(N)
+//Space complexity - O(n)
+public class RemoveLetter {
+    public static void main(String[] args) {
+        String s = "cgandhapplekdif";
+        String f = "apple";
+        System.out.println(remove(s, f));
+    }
+
+    private static String remove(String s, String f) {
+        if(s.isEmpty()){
+            return "";
+        }
+        if(s.startsWith(f)){
+            return remove(s.substring(f.length()), f);
+        }
+        else{
+            return s.charAt(0) + remove(s.substring(1), f);
+        }
+    }
+}
