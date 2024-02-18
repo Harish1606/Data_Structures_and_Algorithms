5700006d50e384e68baeb5b89c845a731ebc0b79
Harish <harishharish3759@gmail.com>
2024-02-18 16:35:42 +0530

    Circular linked list

--- src/com/dsa/linkedlist/Main.java
+++ src/com/dsa/linkedlist/Main.java
@@ -19,13 +19,22 @@ public class Main {
 //        list.delete(1);
 //        list.display();
 
-        DoublyLinkedList list = new DoublyLinkedList();
-        list.insertFirst(3);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertFirst(11);
-        list.insertLast(20);
-        list.insert(17, 63);
+//        DoublyLinkedList list = new DoublyLinkedList();
+//        list.insertFirst(3);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertFirst(11);
+//        list.insertLast(20);
+//        list.insert(17, 63);
+//        list.display();
+
+        CircularLinkedList list = new CircularLinkedList();
+        list.insert(13);
+        list.insert(7);
+        list.insert(4);
+        list.insert(20);
+        list.display();
+        list.delete(7);
         list.display();
     }
 }
