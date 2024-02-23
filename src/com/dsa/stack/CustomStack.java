1e38d695f6d343d1d27e36418d5937f739c78b23
Harish <harishharish3759@gmail.com>
2024-02-23 23:22:23 +0530

    Dynamic stack implementation

--- src/com/dsa/stack/CustomStack.java
+++ src/com/dsa/stack/CustomStack.java
@@ -3,7 +3,7 @@ package com.dsa.stack;
 public class CustomStack {
     protected int[] data;
     private static final int DEFAULT_SIZE = 10;
-    int ptr =- 1;
+    int ptr = -1;
     
     public CustomStack(){
         this(DEFAULT_SIZE);
