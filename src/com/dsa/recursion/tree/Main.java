28e723845593e35113259eaf52d7a96eede4d94c
Harish <harishharish3759@gmail.com>
2024-02-27 22:56:35 +0530

    Binary tree implementation

---
+++ src/com/dsa/recursion/tree/Main.java
@@ -0,0 +1,11 @@
+package com.dsa.recursion.tree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.populate(new Scanner(System.in));
+        binaryTree.display();
+    }
+}
