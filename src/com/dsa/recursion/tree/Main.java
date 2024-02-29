aea116239a4a74510e7fdd366b216e48d536e93b
Harish <harishharish3759@gmail.com>
2024-02-29 23:00:08 +0530

    Binary search tree implementation

--- src/com/dsa/recursion/tree/Main.java
+++ src/com/dsa/recursion/tree/Main.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.populate(new Scanner(System.in));
-        binaryTree.display();
-        binaryTree.prettyDisplay();
+//        BinaryTree binaryTree = new BinaryTree();
+//        binaryTree.populate(new Scanner(System.in));
+//        binaryTree.display();
+//        binaryTree.prettyDisplay();
+
+        int[] nums = {5, 1, 8, 2, 11, 16, 3, 7};
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.populate(nums);
+        binarySearchTree.display();
     }
 }
