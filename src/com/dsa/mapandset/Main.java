b5cb6e4796f9126419b761ec585add506c9f1d8e
Harish <harishharish3759@gmail.com>
2024-04-02 10:44:58 +0530

    Map using linked list implementation

--- src/com/dsa/mapandset/Main.java
+++ src/com/dsa/mapandset/Main.java
@@ -22,6 +22,8 @@ public class Main {
         set.add(13);
         System.out.println(set);
 
+        System.out.println("Map using hash implementation starts ---------->");
+
         MapUsingHash mapUsingHash = new MapUsingHash();
         mapUsingHash.put("harish", "Full stack developer");
         mapUsingHash.put("dinesh", "Social worker");
@@ -29,6 +31,17 @@ public class Main {
         System.out.println(mapUsingHash.get("harish"));
         mapUsingHash.remove("harish");
         System.out.println(mapUsingHash.get("harish"));
+
+        System.out.println("Map using linked list implementation starts ---------->");
+
+        MapUsingLinkedList<String, Integer> mapUsingLinkedList = new MapUsingLinkedList<>();
+        mapUsingLinkedList.put("Harish",100);
+        mapUsingLinkedList.put("Raja", 95);
+        mapUsingLinkedList.put("Dinesh",90);
+        System.out.println(mapUsingLinkedList.get("Raja"));
+        System.out.println(mapUsingLinkedList.containsKey("Harish"));
+        mapUsingLinkedList.remove("Harish");
+        System.out.println(mapUsingLinkedList);
     }
 
 
