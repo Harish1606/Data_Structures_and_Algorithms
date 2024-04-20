c991fcb67b08086c5be877bf509836d45dbd1e14
Harish <harishharish3759@gmail.com>
2024-04-20 11:34:45 +0530

    Stream api map, reduce and findfirst functions

--- src/com/dsa/stream/Main.java
+++ src/com/dsa/stream/Main.java
@@ -1,10 +1,9 @@
 package com.dsa.stream;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-public class ForEach {
+public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
 
@@ -27,7 +26,22 @@ public class ForEach {
         list.forEach(System.out::println);
 
         //Method reference of static method can be implemented as below
-        list.forEach(ForEach::doubleTheValue);
+        list.forEach(Main::doubleTheValue);
+
+        //Sum of all values multiplied by 2 in the list
+        System.out.println(list.stream().map(i -> i*2).reduce(0,Integer::sum));
+
+        List<Integer> list1 = Arrays.asList(12,20,35,46,55,68,75);
+
+        //Sum of all values divisible by 5
+        System.out.println(list1.stream().filter(i -> i%5==0).reduce(0,Integer::sum));
+
+        //Find first element in the list divisible by 5, if there is no element it will return 0
+        System.out.println(list1.stream().filter(i -> i%5==0).findFirst().orElse(0));
+
+        //Map and filter are lazy functions whereas findfirst are terminal functions.
+        //Findfirst will execute before the map and reduce functions, which makes stream api more efficient.
+
     }
 
     public static void doubleTheValue(int i){
